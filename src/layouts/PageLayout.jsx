6117945f7df3b86b589cc40c29e87d24b8927d37
Zevi Liao <jointjm@gmail.com>
2025-07-13 06:10:26 +0800

    Fix refactoring errors

--- src/layouts/PageLayout.jsx
+++ src/layouts/PageLayout.jsx
@@ -1,12 +1,7 @@
 import { Outlet } from "react-router-dom";
-import SuspenseWrapper from "../components/SuspenseWrapper.jsx";
 import "antd/dist/reset.css"; // Import AntD's reset styles
 import "../index.css";
 
 export default function PageLayout() {
-  return (
-    <SuspenseWrapper>
-      <Outlet />
-    </SuspenseWrapper>
-  );
+  return <Outlet />;
 } 
